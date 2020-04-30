09ad6ac3ee162b59eb3efaeeb69215a0a142c03f
Shimouzo <64560725+Shimouzo@users.noreply.github.com>
2020-04-30 17:15:30 +0200

    Delete char_database.py

--- char_database.py
+++
@@ -1,4 +0,0 @@
-chars = "abcdefghijklmnopqrstuvwxyz"
-chars1 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-chars2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-chars3 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&/()=?*;:_>~ˇ^˘°˛`˙´˝¨¸<,.-+'\|€÷×[]łŁß¤@{}§"
